1f35e36e6c5b44e3a59969a5717b6944584359b0
Medo <martinmederly@gmail.com>
2023-05-08 12:37:33 +0200

    fix: change "remove" to "null" value to remove HttpOnly cookies

--- api/rpc_logout_user.go
+++ api/rpc_logout_user.go
@@ -9,10 +9,10 @@ import (
 
 func (server *Server) LogoutUser(ctx context.Context, req *emptypb.Empty) (*emptypb.Empty, error) {
 	md := metadata.Pairs(
-		"X-Access-Token", "remove",
-		"X-Access-Token-Expires-At", "remove",
-		"X-Refresh-Token", "remove",
-		"X-Refresh-Token-Expires-At", "remove",
+		"X-Access-Token", "null",
+		"X-Access-Token-Expires-At", "null",
+		"X-Refresh-Token", "null",
+		"X-Refresh-Token-Expires-At", "null",
 	)
 
 	err := grpc.SendHeader(ctx, md)
