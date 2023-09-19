eb72c05ffb56cdcf6f828ac39c753c3ec02bae1e
Medo <martinmederly@gmail.com>
2023-09-19 21:02:05 +0200

    feat: add new migration - maps

---
+++ db/migration/000013_maps.down.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS "world_locations";
+
+DROP TABLE IF EXISTS "world_maps";
+
+DROP TABLE IF EXISTS "map_pins";
+
+DROP TABLE IF EXISTS "map_layers";
+
+DROP TABLE IF EXISTS "locations";
+
+DROP TABLE IF EXISTS "maps";
