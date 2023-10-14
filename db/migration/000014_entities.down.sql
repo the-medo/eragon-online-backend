c8b0f4643ab603b30daa7a8a25afbd8459e235e9
Medo <martinmederly@gmail.com>
2023-10-14 22:50:22 +0200

    feat: drop table menu_item_entity_groups, add entity_group_id into menu_item table, recreate migrations 14 and 15

--- db/migration/000014_entities.down.sql
+++ db/migration/000014_entities.down.sql
@@ -1,4 +1,14 @@
+DROP PROCEDURE IF EXISTS delete_entity_group(INT);
+DROP PROCEDURE IF EXISTS delete_entity_group_content(INT, INT, INT);
+DROP PROCEDURE IF EXISTS delete_entity_group_content_and_move(INT);
+
+DROP FUNCTION IF EXISTS get_menu_id_of_entity_group(INT);
+DROP PROCEDURE IF EXISTS move_entity_group_content(INT, INT);
+
 DROP FUNCTION get_recursive_entities(_main_entity_group_id integer);
+
+
+ALTER TABLE "menu_items" DROP COLUMN "entity_group_id";
 
 DROP TABLE IF EXISTS "menu_item_entity_groups";
 DROP TABLE IF EXISTS "entity_group_content";
--- db/migration/000015_move_groups_and_their_content.down.sql
+++
@@ -1,7 +0,0 @@
-DROP PROCEDURE IF EXISTS delete_entity_group(INT);
-DROP PROCEDURE IF EXISTS delete_entity_group_content(INT, INT, INT);
-DROP PROCEDURE IF EXISTS delete_entity_group_content_and_move(INT);
-
-DROP FUNCTION IF EXISTS get_menu_id_of_entity_group(INT);
-DROP PROCEDURE IF EXISTS move_entity_group_content(INT, INT);
-DROP PROCEDURE IF EXISTS move_menu_entity_groups(INT, INT, INT);
