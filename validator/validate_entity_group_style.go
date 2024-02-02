ab6506f1277446d5375cf18d31f27092ff26b3e3
Medo <martinmederly@gmail.com>
2024-02-02 21:50:03 +0100

    fix: fix ValidateEntityGroupStyle

--- validator/validate_entity_group_style.go
+++ validator/validate_entity_group_style.go
@@ -1,21 +1,19 @@
 package validator
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/the-medo/talebound-backend/converters"
+	"github.com/the-medo/talebound-backend/pb"
+)
 
 type EntityGroupStyle string
 
-const (
-	Framed    EntityGroupStyle = "framed"
-	NonFramed EntityGroupStyle = "non-framed"
-)
-
 // ValidateEntityGroupStyle checks if the provided string matches any of the EntityGroupStyle values
 func ValidateEntityGroupStyle(value string) error {
-	egs := EntityGroupStyle(value)
-	switch egs {
-	case Framed, NonFramed:
-		return nil
-	default:
+	str := converters.ConvertEntityGroupStyleToPB(value)
+
+	if str == pb.EntityGroupStyle_ENTITY_GROUP_STYLE_UNKNOWN {
 		return fmt.Errorf("invalid entity group style: %s", value)
 	}
+	return nil
 }
