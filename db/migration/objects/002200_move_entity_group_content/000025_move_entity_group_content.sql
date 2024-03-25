5386ee08c4ba5736c080e818a442577253483f16
Medo <martinmederly@gmail.com>
2024-03-25 18:33:35 +0100

    fix: add recursion check into the move_entity_group_content SQL procedure

--- db/migration/objects/002200_move_entity_group_content/000025_move_entity_group_content.sql
+++ db/migration/objects/002200_move_entity_group_content/000025_move_entity_group_content.sql
@@ -3,10 +3,12 @@ CREATE OR REPLACE PROCEDURE move_entity_group_content(p_id INT, p_new_entity_gro
 DECLARE
     v_old_position INT;
     v_old_entity_group_id INT;
+    v_old_content_entity_group_id INT;
     v_new_max_position INT;
+    v_recursion_check INT;
 BEGIN
     -- Get the current position and entity_group_id of the content
-    SELECT "position", "entity_group_id" INTO v_old_position, v_old_entity_group_id
+    SELECT "position", "entity_group_id", "content_entity_group_id" INTO v_old_position, v_old_entity_group_id, v_old_content_entity_group_id
     FROM "entity_group_content"
     WHERE "id" = p_id;
 
@@ -14,6 +16,16 @@ BEGIN
     IF p_new_entity_group_id IS NULL THEN
         p_new_entity_group_id := v_old_entity_group_id;
     end if;
+
+
+    IF v_old_content_entity_group_id IS NOT NULL THEN
+        SELECT COUNT(*) INTO v_recursion_check FROM get_recursive_entities(v_old_content_entity_group_id ) WHERE content_entity_group_id = p_new_entity_group_id;
+
+        -- Check for recursive groups
+        IF v_recursion_check > 0 OR p_new_entity_group_id = v_old_content_entity_group_id THEN
+            RAISE EXCEPTION 'Recursion check failed';
+        END IF;
+    END IF;
 
     IF p_new_entity_group_id != v_old_entity_group_id THEN
 
