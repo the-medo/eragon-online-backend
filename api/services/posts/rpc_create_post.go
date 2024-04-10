439614869137d210b6b16a49dd070305563b0ac5
Medo <martinmederly@gmail.com>
2024-04-10 20:35:45 +0200

    feat: add moduleId property to pbCreatePostRequest

--- api/services/posts/rpc_create_post.go
+++ api/services/posts/rpc_create_post.go
@@ -2,6 +2,8 @@ package posts
 
 import (
 	"context"
+	"database/sql"
+	"github.com/the-medo/talebound-backend/api/servicecore"
 	"github.com/the-medo/talebound-backend/converters"
 	db "github.com/the-medo/talebound-backend/db/sqlc"
 	"github.com/the-medo/talebound-backend/e"
@@ -17,6 +19,10 @@ func (server *ServicePosts) CreatePost(ctx context.Context, req *pb.CreatePostRe
 	if violations != nil {
 		return nil, e.InvalidArgumentError(violations)
 	}
+
+	_, err := server.CheckModuleIdPermissions(ctx, req.GetModuleId(), &servicecore.ModulePermission{
+		NeedsEntityPermission: &[]db.EntityType{db.EntityTypePost},
+	})
 
 	authPayload, err := server.AuthorizeUserCookie(ctx)
 	if err != nil {
@@ -36,6 +42,18 @@ func (server *ServicePosts) CreatePost(ctx context.Context, req *pb.CreatePostRe
 		return nil, status.Errorf(codes.Internal, "failed to create post: %s", err)
 	}
 
+	_, err = server.Store.CreateEntity(ctx, db.CreateEntityParams{
+		Type:     db.EntityTypeLocation,
+		ModuleID: req.GetModuleId(),
+		PostID: sql.NullInt32{
+			Int32: postResult.ID,
+			Valid: true,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
 	post, err := server.Store.GetPostById(ctx, postResult.ID)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to get post: %s", err)
@@ -47,6 +65,9 @@ func (server *ServicePosts) CreatePost(ctx context.Context, req *pb.CreatePostRe
 }
 
 func validateCreatePostRequest(req *pb.CreatePostRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := validator.ValidateUniversalId(req.GetModuleId()); err != nil {
+		violations = append(violations, e.FieldViolation("module_id", err))
+	}
 
 	if err := validator.ValidatePostTitle(req.GetTitle()); err != nil {
 		violations = append(violations, e.FieldViolation("title", err))
