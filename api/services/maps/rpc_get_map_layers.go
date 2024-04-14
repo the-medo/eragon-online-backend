f6a81087c4d2a2d4620e291bb8ba57ac108076ec
Medo <martinmederly@gmail.com>
2024-04-14 22:14:17 +0200

    feat: add permission check and condition to return enabled map layers only

--- api/services/maps/rpc_get_map_layers.go
+++ api/services/maps/rpc_get_map_layers.go
@@ -3,6 +3,7 @@ package maps
 import (
 	"context"
 	"github.com/the-medo/talebound-backend/converters"
+	db "github.com/the-medo/talebound-backend/db/sqlc"
 	"github.com/the-medo/talebound-backend/e"
 	"github.com/the-medo/talebound-backend/pb"
 	"github.com/the-medo/talebound-backend/validator"
@@ -15,17 +16,25 @@ func (server *ServiceMaps) GetMapLayers(ctx context.Context, req *pb.GetMapLayer
 		return nil, e.InvalidArgumentError(violations)
 	}
 
+	returnAllLayers := true
+	_, err := server.CheckEntityTypePermissions(ctx, db.EntityTypeMap, req.GetMapId(), nil)
+	if err != nil {
+		returnAllLayers = false
+	}
+
 	layerRows, err := server.Store.GetMapLayers(ctx, req.GetMapId())
 	if err != nil {
 		return nil, err
 	}
 
 	rsp := &pb.GetMapLayersResponse{
-		Layers: make([]*pb.ViewMapLayer, len(layerRows)),
+		Layers: []*pb.ViewMapLayer{},
 	}
 
-	for i, layerRow := range layerRows {
-		rsp.Layers[i] = converters.ConvertViewMapLayer(layerRow)
+	for _, layerRow := range layerRows {
+		if returnAllLayers || layerRow.Enabled {
+			rsp.Layers = append(rsp.Layers, converters.ConvertViewMapLayer(layerRow))
+		}
 	}
 
 	return rsp, nil
