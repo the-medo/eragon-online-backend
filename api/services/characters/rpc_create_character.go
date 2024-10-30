0e71e983febc89add4d3baa2fcab8aba3bd28416
Medo <martinmederly@gmail.com>
2024-10-30 23:29:17 +0100

    feat: add CreateCharacter and CreateQuest transactions

---
+++ api/services/characters/rpc_create_character.go
@@ -0,0 +1,81 @@
+package characters
+
+import (
+	"context"
+	"database/sql"
+	"github.com/lib/pq"
+	"github.com/the-medo/talebound-backend/converters"
+	db "github.com/the-medo/talebound-backend/db/sqlc"
+	"github.com/the-medo/talebound-backend/e"
+	"github.com/the-medo/talebound-backend/pb"
+	"github.com/the-medo/talebound-backend/validator"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *ServiceCharacters) CreateCharacter(ctx context.Context, req *pb.CreateCharacterRequest) (*pb.CreateCharacterResponse, error) {
+	violations := validateCreateCharacterRequest(req)
+	if violations != nil {
+		return nil, e.InvalidArgumentError(violations)
+	}
+
+	authPayload, err := server.AuthorizeUserCookie(ctx)
+	if err != nil {
+		return nil, e.UnauthenticatedError(err)
+	}
+
+	arg := db.CreateCharacterTxParams{
+		CreateCharacterParams: db.CreateCharacterParams{
+			Name:             req.GetName(),
+			ShortDescription: req.GetShortDescription(),
+			WorldID: sql.NullInt32{
+				Int32: req.WorldId,
+				Valid: true,
+			},
+			SystemID: sql.NullInt32{
+				Int32: req.SystemId,
+				Valid: true,
+			},
+		},
+		UserId: authPayload.UserId,
+	}
+
+	txResult, err := server.Store.CreateCharacterTx(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				return nil, status.Errorf(codes.AlreadyExists, "name already exists: %s", err)
+			}
+		}
+		return nil, status.Errorf(codes.Internal, "failed to create character: %s", err)
+	}
+
+	rsp := &pb.CreateCharacterResponse{
+		Character: converters.ConvertCharacter(*txResult.Character),
+		Module:    converters.ConvertViewModule(*txResult.Module),
+	}
+
+	return rsp, nil
+}
+
+func validateCreateCharacterRequest(req *pb.CreateCharacterRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := validator.ValidateUniversalId(req.GetSystemId()); err != nil {
+		violations = append(violations, e.FieldViolation("system_id", err))
+	}
+
+	if err := validator.ValidateUniversalId(req.GetWorldId()); err != nil {
+		violations = append(violations, e.FieldViolation("world_id", err))
+	}
+
+	if err := validator.ValidateModuleName(req.GetName()); err != nil {
+		violations = append(violations, e.FieldViolation("name", err))
+	}
+
+	if err := validator.ValidateModuleShortDescription(req.GetShortDescription()); err != nil {
+		violations = append(violations, e.FieldViolation("short_description", err))
+	}
+
+	return violations
+}
---
+++ db/sqlc/tx_create_character.go
@@ -0,0 +1,164 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+type CreateCharacterTxParams struct {
+	UserId int32
+	CreateCharacterParams
+}
+
+type CreateCharacterTxResult struct {
+	Character *Character
+	Module    *ViewModule
+}
+
+func (store *SQLStore) CreateCharacterTx(ctx context.Context, arg CreateCharacterTxParams) (CreateCharacterTxResult, error) {
+	var result CreateCharacterTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		character, err := q.CreateCharacter(ctx, arg.CreateCharacterParams)
+		if err != nil {
+			return err
+		}
+
+		menu, err := q.CreateMenu(ctx, CreateMenuParams{
+			MenuCode: "character-" + fmt.Sprint(character.ID),
+			MenuHeaderImgID: sql.NullInt32{
+				Int32: 0,
+				Valid: false,
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		post, err := q.CreatePost(ctx, CreatePostParams{
+			UserID:    arg.UserId,
+			Title:     fmt.Sprintf("%s - introduction", arg.Name),
+			IsDraft:   false,
+			IsPrivate: false,
+			Content:   "",
+		})
+		if err != nil {
+			return err
+		}
+
+		module, err := q.CreateModule(ctx, CreateModuleParams{
+			ModuleType: ModuleTypeCharacter,
+			MenuID:     menu.ID,
+			CharacterID: sql.NullInt32{
+				Int32: character.ID,
+				Valid: true,
+			},
+			DescriptionPostID: post.ID,
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = q.CreateEntity(ctx, CreateEntityParams{
+			Type:     EntityTypePost,
+			ModuleID: module.ID,
+			PostID: sql.NullInt32{
+				Int32: post.ID,
+				Valid: true,
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = q.UpsertUserModule(ctx, UpsertUserModuleParams{
+			ModuleID: module.ID,
+			UserID:   arg.UserId,
+			Admin: sql.NullBool{
+				Bool:  true,
+				Valid: true,
+			},
+			Following: sql.NullBool{
+				Bool:  true,
+				Valid: true,
+			},
+			Favorite: sql.NullBool{
+				Bool:  true,
+				Valid: true,
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = q.CreateMenuItem(ctx, CreateMenuItemParams{
+			MenuID:       menu.ID,
+			MenuItemCode: "overview",
+			Name:         "Overview",
+			Position:     1,
+			IsMain: sql.NullBool{
+				Bool:  true,
+				Valid: true,
+			},
+			DescriptionPostID: sql.NullInt32{},
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = q.CreateModuleAdmin(ctx, CreateModuleAdminParams{
+			ModuleID:           module.ID,
+			UserID:             arg.UserId,
+			SuperAdmin:         true,
+			Approved:           1,
+			MotivationalLetter: "Creator of the character!",
+		})
+
+		if err != nil {
+			return err
+		}
+
+		mapPinTypeGroup, err := q.CreateMapPinTypeGroup(ctx, "Character - "+fmt.Sprint(character.Name))
+		if err != nil {
+			return err
+		}
+
+		_, err = q.CreateModuleMapPinTypeGroup(ctx, CreateModuleMapPinTypeGroupParams{
+			ModuleID:          module.ID,
+			MapPinTypeGroupID: mapPinTypeGroup.ID,
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = q.CreateMapPinType(ctx, CreateMapPinTypeParams{
+			MapPinTypeGroupID: mapPinTypeGroup.ID,
+			Shape:             PinShapePin,
+			BackgroundColor:   sql.NullString{String: "#ffffff", Valid: true},
+			BorderColor:       sql.NullString{String: "#000000", Valid: true},
+			IconColor:         sql.NullString{String: "#000000", Valid: true},
+			Width:             sql.NullInt32{Int32: 24, Valid: true},
+			IsDefault:         sql.NullBool{Bool: true, Valid: true},
+		})
+		if err != nil {
+			return err
+		}
+
+		result = CreateCharacterTxResult{
+			Character: &character,
+			Module: &ViewModule{
+				ID:          module.ID,
+				ModuleType:  ModuleTypeCharacter,
+				CharacterID: sql.NullInt32{Int32: character.ID, Valid: true},
+				MenuID:      menu.ID,
+			},
+		}
+
+		return nil
+	})
+
+	return result, err
+}
